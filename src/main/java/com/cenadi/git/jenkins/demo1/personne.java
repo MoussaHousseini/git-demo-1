746c39b7cfeff980028896bd1cd80ff4730630ff
unknown <moussahousseini@gmail.com>
2023-02-28 14:30:41 -0800

    add to local repose with new class personne

---
+++ src/main/java/com/cenadi/git/jenkins/demo1/personne.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cenadi.git.jenkins.demo1;
+
+/**
+ *
+ * @author Moussa Housseini
+ */
+public class personne {
+   private String personneNom ;
+   private String personnePrenom;
+
+  public personne(){
+   }
+
+    public String getPersonneNom() {
+        return personneNom;
+    }
+
+    public String getPersonnePrenom() {
+        return personnePrenom;
+    }
+
+    public void setPersonneNom(String personneNom) {
+        this.personneNom = personneNom;
+    }
+
+    public void setPersonnePrenom(String personnePrenom) {
+        this.personnePrenom = personnePrenom;
+    }
+    
+}
