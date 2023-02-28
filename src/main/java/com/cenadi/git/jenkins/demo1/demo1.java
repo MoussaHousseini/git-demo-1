9672b587c67f02fe706df91de21398b92195f418
unknown <moussahousseini@gmail.com>
2023-02-28 13:20:43 -0800

    add the project git-jenkins-demo1 to local repo

---
+++ src/main/java/com/cenadi/git/jenkins/demo1/demo1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.cenadi.git.jenkins.demo1;
+
+/**
+ *
+ * @author Moussa Housseini
+ */
+public class demo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("First Git and Jenkins demo tutorial");
+    }
+    
+}
