e59102c9b2ca4a7904040cf47f97dbd2fd0f2fc1
unknown <moussahousseini@gmail.com>
2023-03-01 10:54:36 -0800

    commint againt

--- src/main/java/com/cenadi/git/jenkins/demo1/produit.java
+++ src/main/java/com/cenadi/git/jenkins/demo1/produit.java
@@ -9,5 +9,44 @@ package com.cenadi.git.jenkins.demo1;
  * @author Moussa Housseini
  */
 public class produit {
+
+   private String intitule ;
+   private String quantite;
+   private String description ;
+
+    public produit() {
+    }
+
+    public produit(String intitule, String quantite, String description) {
+        this.intitule = intitule;
+        this.quantite = quantite;
+        this.description = description;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public String getQuantite() {
+        return quantite;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setIntitule(String intitule) {
+        this.intitule = intitule;
+    }
+
+    public void setQuantite(String quantite) {
+        this.quantite = quantite;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
     
 }
